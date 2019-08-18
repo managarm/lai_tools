d1245c5dcae4310e67fd4814d802eefc09a56e2f
mintsuki <mintsuki@protonmail.com>
2019-08-18 21:22:40 +0200

    boot_image: Add 'dump' command to debug prompt

---
+++ boot_image/kernel/include/serial.h
@@ -0,0 +1,8 @@
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+void init_com1(void);
+uint8_t com1_read(void);
+void com1_write(uint8_t data);
+
+#endif
--- boot_image/kernel/src/prompt.c
+++ boot_image/kernel/src/prompt.c
@@ -1,13 +1,47 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <keyboard.h>
 #include <klib.h>
+#include <acpi.h>
+#include <cio.h>
+#include <serial.h>
+#include <lai/host.h>
+
+static uint64_t strtoui(const char *s) {
+    uint64_t n = 0;
+    while (*s)
+        n = n * 10 + ((*(s++)) - '0');
+    return n;
+}
+
+static void dump_sdt(void) {
+    char signature[8];
+    kprint(KPRN_RAW, "table signature: ");
+    gets(signature, 8);
+    char index_s[8];
+    kprint(KPRN_RAW, "table index: ");
+    gets(index_s, 8);
+    size_t index = strtoui(index_s);
+
+    acpi_sdt_t *sdt = laihost_scan(signature, index);
+    kprint(KPRN_RAW, "address of %s is %X\n", signature, sdt);
+    kprint(KPRN_RAW, "%s length: %u bytes\n", signature, sdt->length);
+    kprint(KPRN_RAW, "dumping to serial port...\n");
+    for (size_t i = 0; i < sdt->length; i++)
+        com1_write(((uint8_t *)sdt)[i]);
+    kprint(KPRN_RAW, "done\n");
+}
 
 void debug_prompt(void) {
     kprint(KPRN_RAW, "\nlai_tools debug prompt:\n\n");
 
-    char prompt[256];
+    init_com1();
+
+    char prompt[128];
     for (;;) {
         kprint(KPRN_RAW, ">>> ");
-        gets(prompt, 256);
-        kprint(KPRN_RAW, "%s\n", prompt);
+        gets(prompt, 128);
+        if      (!strcmp(prompt, "dump"))  dump_sdt();
+        else    kprint(KPRN_RAW, "invalid command '%s'\n", prompt);
     }
 }
